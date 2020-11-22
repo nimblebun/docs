f1bc019b700968e7701bf9cf698cf70b2b9a11aa
József Sallai <jozsef@sallai.me>
2020-11-22 23:31:34 +0200

    fix release deployment and URLs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withOptimizedImages = require('next-optimized-images');
 const path = require('path');
 
+const prefix = process.env.APP_PREFIX;
+
 module.exports = withOptimizedImages({
-  basePath: process.env.APP_PREFIX || '',
-  assetPrefix: `${process.env.APP_PREFIX}/` || '/',
+  basePath: prefix || '',
+  assetPrefix: prefix ? `${prefix}/` : '',
   sassOptions: {
     includePaths: [
       path.join(__dirname, 'styles')
